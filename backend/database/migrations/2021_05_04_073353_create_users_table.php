250a85082b09caa3b1b4b175c5808a4a0e1846c3
stwebyy <stweb.yy@gmail.com>
2021-05-04 16:57:49 +0900

    デフォルトのusersテーブルのマイグレーションファイルを削除し、新規マイグレーションファイルの作成

--- backend/database/migrations/2021_05_04_073353_create_users_table.php
+++ backend/database/migrations/2021_05_04_073353_create_users_table.php
@@ -15,13 +15,28 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('first_name_ruby');
+            $table->string('last_name_ruby');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->string('postal_code', 8);
+            $table->tinyInteger('gender');
+            $table->date('birthday');
+            $table->unsignedBigInteger('pref_id');
+            $table->string('city');
+            $table->string('block');
+            $table->string('building');
+            $table->string('phone_number', 15);
             $table->rememberToken();
             $table->dateTime('created_at', 0)->useCurrent();
             $table->dateTime('updated_at', 0)->useCurrent();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('no action');
+            $table->foreign('pref_id')->references('id')->on('pref')->onDelete('no action');
         });
     }
 
