bca4d1291039aa94736923e53ab295ec0b62264e
stwebyy <stweb.yy@gmail.com>
2021-05-04 16:19:21 +0900

    prefテーブルのマイグレーションファイル作成

---
+++ backend/database/migrations/2021_05_04_071645_create_pref_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrefTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pref', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 4);
+            $table->dateTime('created_at', 0)->useCurrent();
+            $table->dateTime('updated_at', 0)->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pref');
+    }
+}
