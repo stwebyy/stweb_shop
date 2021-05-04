55c5f0373514ef4260b08152b5212eb3f706df9b
stwebyy <stweb.yy@gmail.com>
2021-05-04 16:15:47 +0900

    rolesテーブルのマイグレーションファイル作成

---
+++ backend/database/migrations/2021_05_04_070922_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->int('role_id');
+            $table->string('role_name');
+            $table->dateTime('created_at', 0)->useCurrent();
+            $table->dateTime('updated_at', 0)->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
