133219fb8ffdda16d6a6d127b4de3dd57aec02bc
stwebyy <stweb.yy@gmail.com>
2021-05-04 17:46:12 +0900

    orderテーブルのマイグレーションファイルを作成

---
+++ backend/database/migrations/2021_05_04_084336_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('order_status_id');
+            $table->dateTime('created_at', 0)->useCurrent();
+            $table->dateTime('updated_at', 0)->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
