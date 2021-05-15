58c1b986aa34f449b38f84badf22bac02cdcddf1
stwebyy <stweb.yy@gmail.com>
2021-05-16 00:16:46 +0900

    管理者ユーザーのみの認可ミドルウェアを追加

---
+++ backend/app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     * 管理画面ログイン時、Roleが管理者でない場合はリダイレクトさせる
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        #ユーザーがログインしていない場合は、ログイン画面へリダイレクト
+        if (!\Auth::check()) {
+            return redirect()->route('admin_login');
+        }
+ 
+        //ユーザーの権限チェック
+        if (\Auth::user()->role->role_name === '管理者') {
+            return $next($request);
+        } else {
+            return abort(403);
+        }
+    }
+ }
